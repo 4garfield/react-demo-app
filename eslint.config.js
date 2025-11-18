1b9ce5f8c7df884f4959df5206de25a2f82ad438
Garfield <songbintaochina@gmail.com>
2025-11-18 15:18:53 +0800

    add eslint prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,37 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import eslint from '@eslint/js';
+import globals from 'globals';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+import tseslint from 'typescript-eslint';
 
-export default tseslint.config([
-  globalIgnores(['dist']),
+export default defineConfig(
+    globalIgnores([
+    'node_modules/*',
+    'dist/*',
+  ]),
   {
     files: ['**/*.{ts,tsx}'],
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
     extends: [
-      js.configs.recommended,
+      eslint.configs.recommended,
       tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
+      reactPlugin.configs.flat.recommended,
+      reactPlugin.configs.flat['jsx-runtime'],
+      reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
     languageOptions: {
+      parserOptions: { ecmaFeatures: { jsx: true } },
       ecmaVersion: 2020,
       globals: globals.browser,
     },
   },
-])
+  eslintConfigPrettier,
+);
